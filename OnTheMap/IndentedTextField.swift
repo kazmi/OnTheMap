f529b9c2046e0411ea8981bab64284d581ba5f62
Sulaiman Azhar <ssk970@gmail.com>
2015-05-31 19:18:00 +0400

    Add extension to UITextField
    UITextField extension adds an IBInspectable property for left padding.

---
+++ OnTheMap/IndentedTextField.swift
@@ -0,0 +1,20 @@
+//
+//  IndentedTextField.swift
+//  OnTheMap
+//
+//  Created by Sulaiman Azhar on 5/31/15.
+//  Copyright (c) 2015 kazmi. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    @IBInspectable var padding_left: CGFloat {
+        get {
+            return 0
+        }
+        set (f) {
+            layer.sublayerTransform = CATransform3DMakeTranslation(f, 0, 0)
+        }
+    }
+}
