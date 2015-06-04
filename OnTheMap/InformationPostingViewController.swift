a552397af3fa31212fffe6ca5b77f6eef169f715
Sulaiman Azhar <ssk970@gmail.com>
2015-06-04 05:11:44 +0400

    Configure Information Posting View Page UI
    The UI clearly indicates that the user should enter location. The
    location string is converted to latitude and longitude values using the
    CLGeocoder.

--- OnTheMap/InformationPostingViewController.swift
+++ OnTheMap/InformationPostingViewController.swift
@@ -7,17 +7,94 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class InformationPostingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var findButton: UIButton!
+    
+    @IBOutlet weak var linkTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    var geocoder: CLGeocoder!
+    var studyingLocation: CLLocation? = nil
+    
+    var tapRecognizer: UITapGestureRecognizer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        geocoder = CLGeocoder()
+        
+        locationTextField.attributedPlaceholder = NSAttributedString(string: "Enter Your Location Here",
+            attributes: [NSForegroundColorAttributeName:UIColor.whiteColor()])
+        
+        linkTextField.attributedPlaceholder = NSAttributedString(string: "Enter a link to Share Here",
+            attributes: [NSForegroundColorAttributeName:UIColor.whiteColor()])
 
-        // Do any additional setup after loading the view.
+        tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap")
+        tapRecognizer?.numberOfTapsRequired = 1
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardDismissRecognizer()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardDismissRecognizer()
     }
     
     @IBAction func cancelInformationPosting(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func findOnTheMap(sender: AnyObject) {
+        geocoder.geocodeAddressString(locationTextField.text,
+            completionHandler: { (placemarks:[AnyObject]!, error: NSError!) -> Void in
+                
+            if error == nil && placemarks.count > 0 {
+                let placemark = placemarks[0] as! CLPlacemark
+                self.studyingLocation = placemark.location
+                
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.cancelButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+                    self.questionLabel.hidden = true
+                    self.locationTextField.hidden = true
+                    self.findButton.hidden = true
+                    self.linkTextField.hidden = false
+                    self.mapView.hidden = false
+                    self.submitButton.hidden = false
+                })
+                
+            }
+         })
+    }
+    
+    @IBAction func postInformation(sender: AnyObject) {
+        
+    }
+    
+    
+    //#MARK:- Keyboard Fixes & Notifications
+    
+    func addKeyboardDismissRecognizer() {
+        view.addGestureRecognizer(tapRecognizer!)
+    }
+    
+    func removeKeyboardDismissRecognizer() {
+        view.removeGestureRecognizer(tapRecognizer!)
+    }
+    
+    func handleSingleTap() {
+        view.endEditing(true)
     }
     
 
